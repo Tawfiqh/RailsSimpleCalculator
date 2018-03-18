e42e2824211ea3f4ee719105309331f8448500ba
Tawfiq Hamid <tawfiq.hamid@gmail.com>
2018-03-18 22:45:23 +0000

    Removed files and added css for calculator-body width

--- test/controllers/calculator_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CalculatorControllerTest < ActionDispatch::IntegrationTest
-  test "should get basic1" do
-    get calculator_basic1_url
-    assert_response :success
-  end
-
-end
